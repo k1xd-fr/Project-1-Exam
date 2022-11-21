2be911a7d5f6b3aa66474b7f6bea45e95a5e9604
Junusov Almaz <junusov477@gmail.com>
2022-11-21 12:28:52 +0600

    add  info main aside-text

--- setting/js/main.js
+++ setting/js/main.js
@@ -1,1 +1,12 @@
 
+ // Look for .hamburger
+ var hamburger = document.querySelector(".hamburger");
+ var navbar__strokes = document.querySelector(".navbar__strokes");
+ // On click
+ hamburger.addEventListener("click", function () {
+   // Toggle class "is-active"
+   hamburger.classList.toggle("is-active");
+
+   navbar__strokes.classList.toggle("is-active");
+   // Do something else, like open/close menu
+ });
